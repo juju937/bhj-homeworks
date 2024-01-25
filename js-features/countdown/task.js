b57d9f0249483dbc8daaeda4e3677dbc5807b91a
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2024-01-25 23:16:17 +0300

    countdown task fixed

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,5 +1,9 @@
-let timer = document.getElementById('timer');
+const seconds = 59;
 
 setInterval(function() {
-  timer -= 1;
+  seconds -= 1;
+    if(seconds == 0){
+      alert('Вы победили в конкурсе!');
+    }
+    document.getElementById("timer").innerText = seconds;
 }, 1000);
