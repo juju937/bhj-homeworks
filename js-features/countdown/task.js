4dd2e32edbafd1cdb0d52f9876926ead4eea2bb3
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2024-01-25 23:23:25 +0300

    clear interval added

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -3,6 +3,7 @@ let seconds = 59;
 setInterval(function() {
   seconds -= 1;
     if(seconds == 0){
+      clearInterval(seconds);
       alert('Вы победили в конкурсе!');
     }
     document.getElementById("timer").innerText = seconds;
