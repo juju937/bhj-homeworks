43b8efcb3d5816495d064b38a60f06756f548139
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2024-01-26 00:46:36 +0300

    win/lose fixed

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -5,23 +5,25 @@ let holes = document.querySelectorAll(".hole");
 const field = Array.from(holes);
 
 field.forEach((hole) => {
-hole.addEventListener("click", () => {
-  if (hole.classList.contains('hole_has-mole')) {
-    molesDead++;
-    document.getElementById("dead").innerText = molesDead;
-  } else {
-    molesMissed++;
-    document.getElementById("lost").innerText = molesMissed;
-  }
-  });
-});
 
-if (molesDead === 10) {
-  alert('Вы победили!');
-  molesDead = 0;
-  molesMissed = 0;
-} else if (molesMissed === 5) {
-  alert('Вы проиграли!');
-  molesDead = 0;
-  molesMissed = 0;
-}
+  hole.addEventListener("click", () => {
+
+    if (hole.classList.contains('hole_has-mole')) {
+      molesDead++;
+      document.getElementById("dead").innerText = molesDead;
+    } else {
+      molesMissed++;
+      document.getElementById("lost").innerText = molesMissed;
+    }
+
+    if (molesDead === 10) {
+      alert('Вы победили!');
+      molesDead = 0;
+      molesMissed = 0;
+    } else if (molesMissed === 5) {
+      alert('Вы проиграли!');
+      molesDead = 0;
+      molesMissed = 0;
+    }
+  })
+})
