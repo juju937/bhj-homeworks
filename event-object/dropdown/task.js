1f69668406bb562e9a1654e1bb4d9eced912ab48
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2024-02-01 02:11:40 +0300

    ver 4

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,5 +1,14 @@
 let dropdownButton = document.querySelector('.dropdown__value');
 
 dropdownButton.addEventListener("click", () => {
-  document.querySelector('.dropdown__list').classList.toggle("dropdown__list_active");
+  document.querySelector('.dropdown__list').classList.add("dropdown__list_active");
 });
+
+let dropdownItem = Array.from(document.querySelectorAll('.dropdown__item'));
+
+dropdownItem.forEach((item) => {
+item.addEventListener("click", () => {
+  document.querySelector('.dropdown__list').classList.remove("dropdown__list_active");
+  this.closest('.dropdown__value').children[0].textContent = this.textContent;
+})
+})
