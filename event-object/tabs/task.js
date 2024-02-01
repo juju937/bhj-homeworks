1460218b08a1d769b44f55330f32348a953a3aab
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2024-02-01 03:13:52 +0300

    ver 2

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,9 +1,13 @@
 let tabHeaders = Array.from(document.querySelectorAll('.tab'));
 
 
-tabHeaders.forEach((tab) => {
+tabHeaders.forEach((tab, index) => {
 tab.addEventListener("click", () => {
+  if (tab.classList.contains("tab_active")) {
+    tab.classList.remove("tab_active");
+  }
   tab.classList.add("tab_active");
-  document.querySelector('.tab__content').classList.add("tab__content_active");
+  document.querySelector('.tab__contents>.tab__content_active').classList.remove('tab__content_active');
+  document.querySelector(`.tab__contents>:nth-child(${index + 1})`).classList.add('tab__content_active');
 });
 });
