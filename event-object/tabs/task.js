7337a2f0a7f2a2a7ae08a2db9de038e787dcd340
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2024-02-01 03:17:52 +0300

    ver 2 fixed

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -3,10 +3,8 @@ let tabHeaders = Array.from(document.querySelectorAll('.tab'));
 
 tabHeaders.forEach((tab, index) => {
 tab.addEventListener("click", () => {
-  if (tab.classList.contains("tab_active")) {
-    tab.classList.remove("tab_active");
-  }
-  tab.classList.add("tab_active");
+  document.querySelector('.tabs>.tab_active').classList.remove('tab_active');
+  document.querySelector(`.tabs>:nth-child(${index + 1})`).classList.add('tab_active');
   document.querySelector('.tab__contents>.tab__content_active').classList.remove('tab__content_active');
   document.querySelector(`.tab__contents>:nth-child(${index + 1})`).classList.add('tab__content_active');
 });
