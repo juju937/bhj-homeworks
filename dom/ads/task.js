088be63f21c196c8cbab25bb70e1cb4de42d2188
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2024-02-07 01:34:34 +0300

    rotator assignment v1

---
+++ dom/ads/task.js
@@ -0,0 +1,13 @@
+let rotator = document.querySelectorAll('.rotator__case');
+
+setInterval(()=>{
+  rotator.forEach((element, index) => {
+    const currentlyActive = document.querySelector(".rotator__case.rotator__case_active");
+    if (currentlyActive) {
+      currentlyActive.classList.remove('rotator__case_active');
+    }
+    element.classList.add('rotator__case_active');
+    document.querySelector('.rotator>.rotator__case_active').classList.remove('rotator__case_active');
+    document.querySelector(`.rotator>:nth-child(${index + 1})`).classList.add('rotator__case_active');
+  })
+}, 1000);
