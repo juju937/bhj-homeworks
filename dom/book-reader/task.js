4bab1067d991a1938b16a77740b767459747af08
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2024-02-11 00:47:58 +0300

    book reader fixed

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -3,19 +3,23 @@ let book = document.getElementById('book');
 
 fontSizeButtons.forEach((item) => {
   item.addEventListener('click', function(event) {
-    event.preventDefault()
-    let currentlySelectedSize = document.querySelector(".font-size.font-size_active");
-      if (currentlySelectedSize) {
-        currentlySelectedSize.classList.remove("font-size_active");
-      }
-    item.classList.add("font-size_active");
-    
-    if (item.classList.contains('font-size_small')) {
-      book.classList.add('book_fs-small')
-    }
+    event.preventDefault();
 
-    if (item.classList.contains('font-size_big')) {
-      book.classList.add('book_fs-big')
+    let currentlySelectedSize = document.querySelector(".font-size.font-size_active");
+    if (currentlySelectedSize) {
+      currentlySelectedSize.classList.remove("font-size_active");
     }
-  })
-})
+    item.classList.add("font-size_active");
+
+    if (item.classList.contains('font-size_small')) {
+      book.classList.remove('book_fs-big');
+      book.classList.add('book_fs-small');
+    } else if (item.classList.contains('font-size_big')) {
+      book.classList.remove('book_fs-small');
+      book.classList.add('book_fs-big');
+    } else {
+      book.classList.remove('book_fs-small');
+      book.classList.remove('book_fs-big');
+    }
+  });
+});
